37aeec5d458245937cec0793c74429c477e08710
sceptre12 <xthom001@corellia.cs.fiu.edu>
2014-02-21 02:59:18 -0500

    Multiple changes have been made so 1. Please read the read me that will instruct you on we're going to solve this git hub pulling and pushing issue 2. I've added containers in the page and add a link to the second page, we'll need someone to start working on the gallery page which has all the past and fellow mbrs students 3. we are working soley on the mbrs side of things because after that is finished all thats left is to copy those files and change them to marc u star and add in the appropriate changes 4. i have to fix the title instead of using h1 tags i just used a div to set the size for the home page but that messed with the media's you'll see when you resize the page

---
+++ TesterPHP.php
@@ -0,0 +1,6 @@
+The Tester fles are to be copied and stored on your own computer. any changes you make do it in them and then ask around to see if anyone has recently pushed a change.
+If no one has pushed a change, copy whatever changes you've made out of these tester files, paste them in the tester files on your own desktop, then type in "git stash"(1)
+after type in "git pull origin master" (2) after which you will have the newest changes... Check to make sure everything is in order after which, copy your changes into the appropriate feilds. 
+Then type in "git stash clear"(3). After that then type in "git add ."(4) "git commit -m "[description of of what changes you've done]"" (5) then "git push origin master"
+
+- Check read me for references 
