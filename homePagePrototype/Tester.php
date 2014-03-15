b90a30261eb0fe06278a26c198e95e760c3912c2
Nickbar <nicoba6@yahoo.com>
2014-03-14 20:10:29 -0400

    created navbar for marc u, etc

---
+++ homePagePrototype/Tester.php
@@ -0,0 +1,266 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>MBRS &amp; MARC-U-STAR</title>
+
+    <!-- Bootstrap Core CSS -->
+    <script type="text/javascript" src="jquery.js"></script>
+    
+    <!-- Fonts -->
+    <link href="http://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href='http://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
+
+    <!-- Custom Theme CSS -->
+
+    <link href="css/grayscale.css" rel="stylesheet">
+    <!-- why is this being loaded it sholdnt be loaded -->
+    <!-- <link href="Tester2.css" rel="stylesheet"> -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    
+    
+
+</head>
+
+<body id="page-top" data-spy="scroll" data-target=".navbar-custom">
+
+<?php include "navbar.html"; ?>
+
+    
+<section id="Mbrs_Section1">
+        
+                    
+                    <div id="Mbrs_carousel" class="carousel slide" style="width: 100% auto; margin: 0 auto; height: 600px auto; overflow: hidden" >
+                        <!-- title for the carousel  -->
+                        <!-- removed for being ugly -->
+                        <div id="Mbrs_TitleContainer">
+                            <div id="Mbrs_carouselTitle">
+                                <b>Welcome to FIU's MBRS Home Page</b> 
+                            </div>
+                        </div>
+
+                        <!-- A little information about the program  -->
+
+                        
+                          <!-- Wrapper for slides -->
+                        <div class="carousel-inner">
+                            <div class="item active">
+                                <img src="img/filler1.jpg" alt="Beach" class ="img-responsive">
+                                <div class ="carousel-caption">
+                                    <div class="Mbrs_IntroTextBox">
+                            <div class="Mbrs_IntroTextStyle">
+                                <b>MBRS</b> is a student development program for minority
+        serving institutions. The program supports institutional grants with well integrate developmental activities that may include, but are not limited to, research experiences in on campus laboratories, collaborative learning experiences, research careers seminars, scientific reading comprehension and writing skills, tutoring for excellence, and travel to scientific meetings.
+        
+                            </div>
+                        </div>
+                                    <a href="#Mbrs_Section2"><span class="glyphicon glyphicon-chevron-down " style="font-size: 56px; color: white; opacity: 0.7"></span></a>
+                                </div>
+                            </div>
+                            
+                            <div class="item ">
+                                <img src="img/filler2.jpg" alt="Beach" class ="img-responsive">
+                                <div class ="carousel-caption">
+                                    <div class="Mbrs_ImgContainter">
+                                        <img src="img/Symposium.png">
+                                    </div>
+
+                                    <div class="Mbrs_IntroTextBox">
+                            <div class="Mbrs_IntroTextStyle">
+                                <b>MBRS MINI
+-
+SYMPOSIUM</b> <br>
+1st Place Undergraduate Students Presentation
+—
+<b>Ruslan Garcia</b>,<br>
+MARC U*STAR Undergraduate Fellow
+Major: Biological Sciences, College of Arts and Sciences
+Faculty Mentor: Dr. Lidia Kos
+Presentation Title
+: Systematic assessment of quantification
+methods for low pathologic avian influenza virus in surveillance
+samples {Summer Research with Dr. Richard Webby, St. Jude
+Children’s Research Hospital}
+        
+                            </div>
+                        </div>
+                                    <a href="#Mbrs_Section2"><span class="glyphicon glyphicon-chevron-down " style="font-size: 56px; color: white; opacity: 0.7"></span></a>
+                                </div>
+                            </div>
+
+                            <div class="item ">
+                                <img src="img/filler3.jpg" alt="Beach" class ="img-responsive">
+                                <div class ="carousel-caption">
+                                    <div class="Mbrs_ImgContainter">
+                                        <img src="img/Symp2.png">
+                                    </div>
+
+                                    <div class="Mbrs_IntroTextBox">
+                            <div class="Mbrs_IntroTextStyle">
+                                <b>MBRS MINI
+-
+SYMPOSIUM</b><br> 1st Place Graduate Student Presentation
+—
+<b>Maria Areiza</b>,<br> MBRS
+RISE Graduate Fellow
+Department: Biological Sciences, College of Arts and Sciences
+Faculty Mentor: Dr. Fernando Noriega
+Presentation Title:
+Ecdysis Triggering Hormone (ETH) and its
+Role in Juvenile Hormone Synthesis in the mosquito,
+Aedes
+aegypti
+        
+                            </div>
+                        </div>
+                                    <a href="#Mbrs_Section2"><span class="glyphicon glyphicon-chevron-down " style="font-size: 56px; color: white; opacity: 0.7"></span></a>
+                                </div>
+                            </div>
+
+                            <div class="item ">
+                                <img src="img/filler4.jpg" alt="Beach" class ="img-responsive">
+                                <div class ="carousel-caption">
+                                    <a href="#Mbrs_Section2"><span class="glyphicon glyphicon-chevron-down " style="font-size: 56px; color: white; opacity: 0.7"></span></a>
+                                </div>
+                            </div>
+
+                        </div>
+                            
+                            <!-- I took off the glyphicons just to see how it looks and i kind of like it let me know what you all think.. granted we can make the whole glyphicons box smaller but i like the idea of when you hover your mouse to the side, the side darkens -->
+
+                          
+                          <a  class="left carousel-control" href="#Mbrs_carousel" data-slide="prev">
+                            <!-- <span class="glyphicon glyphicon-chevron-left"></span> -->
+                          </a>
+                          <a class="right carousel-control" href="#Mbrs_carousel" data-slide="next">
+                            <!-- <span class="glyphicon glyphicon-chevron-right"></span> -->
+                          </a>
+                </div>
+    </section>
+
+    <section id="Mbrs_Section2" style="background-color: #c5960c" >
+         <div class="gloss">
+        <div class="container-fluid">
+    <div class="row" style="border-width = 2px; ">
+        <div class="col-lg-12">
+            <div class="row">
+                <center style="font-size: 45px; color: rgba(0, 41, 88, 0.901961)">
+                <b>MBRS News &amp; Events</b></center>
+            </div>
+            <div class="row">
+                <div class="col-sm-3">
+                    <b><center>15th Annual Biomedical &amp; Comparative Immunology (BCI) Symposium,
+                    February 13 &amp; 14th, 2014
+                    <br><br>Please click below to Register<br>
+                    <a href="https://www.eventbrite.com/e/16th-annual-biomedical-and-comparative-immunology-symposium-tickets-10083011559?ref=ebtnebregn"><button type="button" style="background-color: rgba(0, 41, 88, 0.901961); border-color: #000000;"class="btn btn-info">Click Me</button></a>
+                    </center></b>
+                </div>
+                <div class="col-sm-6">
+                    <div class = "row" >
+                            <div class="col-sm-12">
+                                <u><b><center>MBRS RISE PROGRAM NOW ACCEPTING APPLICATIONS</center></b></u>
+                            </div>
+                    </div>
+                    <div class="row">
+                            <div class="col-md-6">
+                                <b><center>Graduate Application Deadline<br>
+                                January 31st, 2014</b>
+                                <br>
+                                <a href="http://mbrs.fiu.edu/rise/application.shtml"><button type="button" style="background-color: rgba(0, 41, 88, 0.901961); border-color: #000000;" class="btn btn-info">Sign Up</button></a></center>
+                            </div>
+                            <div class="col-md-6">
+                                <b><center>Undergraduate Application Deadline
+                                <br>February 28,2014</b><br>
+                                <a href="http://mbrs.fiu.edu/rise/application.shtml"><button type="button" style="background-color: rgba(0, 41, 88, 0.901961); border-color: #000000;" class="btn btn-info">Sign Up</button></a></center><br>
+                            </div>
+                            <div class="row">
+                                <div class="col-md-12">
+                                    <b><center>For More News &amp; Events Please click below<br>
+                                    <a href="http://mbrs.fiu.edu/rise/application.shtml"><button type="button" style="background-color: rgba(0, 41, 88, 0.901961); border-color: #000000;" class="btn btn-info">Sign Up</button></a></center></b>
+                                </div>
+                            </div>
+                    </div>
+                </div>
+                <div class="col-sm-3">
+                            <b><center>MBRS RISE Biomedical Research Initative (BRI) 2014 FIU Student Summer Research Awards
+                                <br><br>Applications Due by<br>
+                            Friday, March 7th, 2014 at 5pm
+                            <br>For more information click below</b>
+                            <br>
+                            <a href="http://mbrs.fiu.edu/docs/2014%20BRI%20Application%20Guidelines.pdf"><button type="button" style="background-color: rgba(0, 41, 88, 0.901961); border-color: #000000;"class="btn btn-info"> Click Me</button></a></center>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</div>
+</section>
+
+    <section id="Mbrs_Section3" >
+    
+    <div class="container-fluid" >
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="row">
+                    <div class="col-lg-12" style="background-color: rgba(0, 41, 88, 0.901961)">
+                        <center style="font-size: 45px; color: #daa520"><b>Program Description</b></center>
+                    </div>
+                </div>
+                
+                <div class="row" style="background-color: #c5960c; ">
+                    <div class="gloss">
+                    <div id="Mbrs-padding"class="col-lg-12" >
+                       
+                        <center>
+                <p >The Minority Biomedical Research Support Research Initiative for Scientific
+Enhancement
+(MBRS RISE) program, sponsored by a National Institutes of Health
+(NIH) grant, is aimed at increasing the number of faculty, students and investigators
+who are members of groups that are underrepresented in the biomedical sciences.
+MBRS RISE grants are awarded to 2
+-
+or 4
+-
+year colleges, universities and health profes-
+sional schools with 50 percent or more student enrollment from underrepresented
+groups to support research by faculty members, strengthen the institutions' biomedi-
+cal research capabilities and provide opportunities for students to work as part of a
+research team. The FIU MBRS RISE Program provides graduate and undergraduate students compensation for a yearlong work obligation in a laboratory conducting biomedical research. Sponsored by the National Institutes of Health (NIH), the MBRS RISE Program is dedicated to increasing the number of undergraduate and graduate students from underrepresented groups in biomedical and behavioral research to enter into and successfully complete M.D./Ph.D. or Ph.D.
+programs. Research travel funds are available for the MBRS RISE students to attend scientific meetings and conferences. The program also provides a tuition-waiver for graduate students. 
+Other training includes: special skills &amp; professional development workshops as well as academic advisement. Undergraduate students may apply during their freshman, sophomore or junior years for participation in the program from the time of acceptance through their senior year. Graduate students may apply at any point during their FIU graduate program.</center>
+ </p> 
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</section>
+
+    <footer></footer>
+
+    <script src="https://code.jquery.com/jquery-1.10.2.min.js"></script>
+    <script src="js/grayscale.js"></script>
+     <script src="css/dist/js/bootstrap.js"></script>
+     <script src="css/dist/js/Tooltip.js"></script>
+     <script src="css/dist/js/Popover.js"></script>
+     <script>
+        $(function()
+        {
+                $('#example').popover();
+        });
+     </script>
+    </body>
+</html>
+
+<!-- this is for making a pop up screen i'm thinking of adding this some where in the site -->
+
+    <!-- <center><a id="example"  class="btn btn-lg btn-danger" role="button" data-content="right?" title="" data-toggle="popover" href="#" data-original-title="Title" data-placement="bottom">
+      Click to Access Current Events 
+    </a></center> -->
