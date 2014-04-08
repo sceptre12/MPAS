fbb9772c388241b48dd7b5d8485e8ff77011297f
Xavier <xthom001@gmail.com>
2014-04-07 22:35:19 -0400

    Fixed minor bugs in the marccurriculum.php page going through the rest of the site now

--- homePagePrototype/marccurriculum.php
+++ homePagePrototype/marccurriculum.php
@@ -36,32 +36,33 @@
                     <div >
                         <!-- I need to fix the title - Xavier...Implemented
                                 seems fine(Daryl)-->
-                            <center id="HomeMS_Title"><style type="text/css" ></style>MARC U*STAR</center>
+                            <center id="HomeMS_Title">MARC U*STAR</center>
                     </div>
                 </div>
-        </div>
-            <div class="container">
-                <div class="row">
-                    <div class="col-md-12">
-                        <h1 class="brand-heading">Welcome</h1>
-                            <div class="row">
-                                <div class="col-md-4">
-                                    <div class="HomeMS_Sect1_ButtAlign">
-                                    </div>
-                                </div>
+            </div>
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-12">
+                            <h1 class="brand-heading">Welcome</h1>
+                                <div class="row">
                                     <div class="col-md-4">
-                                    <p class="intro-text">TO THE MARC U*STAR CURRICULUM</p>
-                                        <div class="page-scroll" style="padding-bottom:20px">
-                                            <a href="#about" class="btn-circle">
-                                                <i class="fa fa-angle-double-down animated"></i>
-                                            </a>
+                                        <div class="HomeMS_Sect1_ButtAlign">
                                         </div>
-                                     </div>   
-                                <div class="col-md-4">
-                                    <div class="HomeMS_Sect1_ButtAlign">
+                                    </div>
+                                        <div class="col-md-4">
+                                        <p class="intro-text">TO THE MARC U*STAR CURRICULUM</p>
+                                            <div class="page-scroll" style="padding-bottom:20px">
+                                                <a href="#about" class="btn-circle">
+                                                    <i class="fa fa-angle-double-down animated"></i>
+                                                </a>
+                                            </div>
+                                         </div>   
+                                    <div class="col-md-4">
+                                        <div class="HomeMS_Sect1_ButtAlign">
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -110,34 +111,37 @@
     <section id="HomeMS_Section4" style="position:relative" >
         <div class="container" style="padding-top:100px; padding-left: 150px;">
             <div class="row">
-              <div class="col-sm-6">
-                <img src="img/marc1.png" style="width:105%; height:105%">
-              </div>
-              <div class="col-sm-6" style="color:white">
-                    <ul>
-                        <li>Orientation to the FIU MARC U*STAR Program and Faculty/Labs </li>
-                        <li>Preparing for Graduate School</li>
-                            <ul>
-                                <li>Info sessions with various representatives from grad programs across the nation  </li>
-                            </ul>
-                        <li>Study Skills and Resources</li>
-                        <li>CPR/AED Certification</li>
-                        <li>First Aid Certification</li>
-                        <li>Preparing Scientific Presentations</li>
-                        <li>Data Bases & Library Searches</li>
-                        <li>Responsible Conduct of Research</li>
-                        <li>Lab Safety Workshops</li>
-                             <ul>
-                                <li>EPA: Hazardous Waste Handling</li>
-                                <li>Environmental Awareness PT 2: waste and chemical</li>
-                                <li>Small Spills and Leaks</li>
-                            </ul> 
-                        <li>Scientific Concept Mapping</li>
-                        <li>Communications Skills Workshop</li>      
-              </div>
+                <div class="col-sm-6">
+                    <img src="img/marc1.png" style="width:105%; height:105%">
+                </div>
+                <div class="col-sm-6" style="color:white">
+                        <ul>
+                            <li>Orientation to the FIU MARC U*STAR Program and Faculty/Labs </li>
+                            <li>Preparing for Graduate Schools
+                                <ul>
+                                    <li>Info sessions with various representatives from grad programs across the nation  </li>
+                                </ul>
+                            </li>
+                            <li>Study Skills and Resources</li>
+                            <li>CPR/AED Certification</li>
+                            <li>First Aid Certification</li>
+                            <li>Preparing Scientific Presentations</li>
+                            <li>Data Bases & Library Searches</li>
+                            <li>Responsible Conduct of Research</li>
+                            <li>Lab Safety Workshops
+                                 <ul>
+                                    <li>EPA: Hazardous Waste Handling</li>
+                                    <li>Environmental Awareness PT 2: waste and chemical</li>
+                                    <li>Small Spills and Leaks</li>
+                                </ul> 
+                            </li>
+                            <li>Scientific Concept Mapping</li>
+                            <li>Communications Skills Workshop</li>
+                        </ul>      
+                </div>
             </div>
         </div>    
-           <div class="page-scroll text-center">
+            <div class="page-scroll text-center">
                 <a href=".opportunities" class="btn-circle">
                     <i class="fa fa-angle-double-down animated"></i>
                 </a>
@@ -155,7 +159,7 @@
         </div>
     </div>
 
-    <section id="HomeMS_Section4" style="position:relative" >
+    <section id="HomeMS_Section5" style="position:relative" >
         <div class="container" style="padding-top:100px; padding-left: 150px;">
             <div class="row">
               <div class="col-sm-6">
@@ -167,8 +171,9 @@
                         <li>Annual Biomedical Conference for Minority Students (fall)</li>
                         <li>Biological Sciences Research Life Symposium (spring)</li>
                         <li>Careers in Science: Undergraduate Student Conference (spring)</li>
-                        <li>Biomedical & Comparative Immunology Symposium (spring)</li>
-                        <li>And many more conferences…</li>     
+                        <li>Biomedical &amp; Comparative Immunology Symposium (spring)</li>
+                        <li>And many more conferences…</li>  
+                    </ul>   
               </div>
             </div>
         </div>    
@@ -224,7 +229,7 @@
     <div class="about-us">
         <div class="container">
             <div class="col-lg-8 col-lg-offset-2">
-                <h3>The major student research training effort takes place at designated laboratories. MARC U*STAR Fellows are trained in laboratory techniques, data collection and analysis, as well as in literature searches. MARC U*STAR Fellows give an oral presentation at the annual FIU MBRS Mini-Symposium and are involved in all phases of the research process.<h3>
+                <h3>The major student research training effort takes place at designated laboratories. MARC U*STAR Fellows are trained in laboratory techniques, data collection and analysis, as well as in literature searches. MARC U*STAR Fellows give an oral presentation at the annual FIU MBRS Mini-Symposium and are involved in all phases of the research process.</h3>
             </div>
         </div>
         <div class="page-scroll text-center">
@@ -248,7 +253,7 @@
     <div class="about-us">
         <div class="container">
             <div class="col-lg-8 col-lg-offset-2">
-                <h3>Scientific seminars increase MARC U*STAR Fellows' exposure to biomedical research. These seminars include the departmental series in Biology, Physics, Chemistry and Biochemistry, etc. as well as the seminars sponsored, for example, by the MARC U*STAR Program, MBRS Program, FIU Biomedical Engineering Program, and the FIU Comparative Immunology Group.<h3>
+                <h3>Scientific seminars increase MARC U*STAR Fellows' exposure to biomedical research. These seminars include the departmental series in Biology, Physics, Chemistry and Biochemistry, etc. as well as the seminars sponsored, for example, by the MARC U*STAR Program, MBRS Program, FIU Biomedical Engineering Program, and the FIU Comparative Immunology Group.</h3>
             </div>
         </div>
         <div class="page-scroll text-center">
@@ -269,7 +274,7 @@
         </div>
     </div>
 
-        <section id="HomeMS_Section4" style="position:relative" >
+        <section id="HomeMS_Section6" style="position:relative" >
         <div class="container" style="padding-top:100px; padding-left: 150px;">
             <div class="row">
               <div class="col-sm-6">
